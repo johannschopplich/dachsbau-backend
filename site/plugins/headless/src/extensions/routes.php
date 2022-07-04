f982b0c7a84fbfeb9d07c46ea62dec50c8309453
Johann Schopplich <mail@johannschopplich.com>
2022-07-04 11:19:44 +0200

    refactor: update headless kit

---
+++ site/plugins/headless/src/extensions/routes.php
@@ -0,0 +1,29 @@
+<?php
+
+use KirbyHeadless\Api\Api;
+use KirbyHeadless\Api\Middlewares;
+use KirbyHeadless\Api\UserMiddlewares;
+
+return [
+    /**
+     * Allow preflight requests, mainly for `fetch`
+     */
+    [
+        'pattern' => '(:all)',
+        'method' => 'OPTIONS',
+        'action' => fn () => Api::preflightResponse()
+    ],
+
+    /**
+     * Return JSON-encoded page data for each request
+     */
+    [
+        'pattern' => '(:all)',
+        'action' => Api::createHandler(
+            [Middlewares::class, 'tryResolveFiles'],
+            [UserMiddlewares::class, 'hasAuthHeaderOrRedirect'],
+            [Middlewares::class, 'hasBearerToken'],
+            [Middlewares::class, 'tryResolvePage']
+        )
+    ]
+];
