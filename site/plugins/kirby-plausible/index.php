e6b6e73c0cde1a4393f01bb1e07fa54eb7e3a59a
Johann Schopplich <mail@johannschopplich.com>
2023-01-30 21:06:02 +0100

    chore: untrack Kirby plugins from repo

--- site/plugins/kirby-plausible/index.php
+++
@@ -1,8 +0,0 @@
-<?php
-
-\Kirby\Cms\App::plugin('johannschopplich/kirby-plausible', [
-    'areas' => require __DIR__ . '/src/extensions/areas.php',
-    'snippets' => [
-        'plausible' => __DIR__ . '/src/snippets/plausible.php'
-    ]
-]);
--- site/plugins/kirby-plausible/src/snippets/plausible.php
+++
@@ -1,3 +0,0 @@
-<?php if ($kirby->option('debug') === false && $kirby->user() === null): ?>
-  <script defer data-domain="<?= $kirby->option('johannschopplich.plausible.domain', parse_url($kirby->url())['host']) ?>" src="https://plausible.io/js/plausible.js"></script>
-<?php endif ?>
